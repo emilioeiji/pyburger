dd4d51fde776d3d9f333737f4d2b18ffce5a681b
emilioeiji <emilioeiji@gmail.com>
2023-05-05 20:11:14 +0900

    Criando model produtos

--- burguer/urls.py
+++ burguer/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -5,4 +7,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
